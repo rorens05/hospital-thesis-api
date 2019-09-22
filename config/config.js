a0114089776f49ccdfe7d88fbc9745abbc79d773
Laurence Buatista <rorens05@gmail.com>
2019-09-23 00:36:07 +0800

    testing

--- config/config.js
+++ config/config.js
@@ -18,7 +18,7 @@ module.exports = {
   "production": {
     "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME,
+    "database": "heroku_3fcb7241bb24302",
     "host": process.env.DB_HOST,
     "dialect": "mysql"
   }
