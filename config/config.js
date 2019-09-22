0ddbed7d12553b7349fe656f1e5873b4e875e58e
Laurence Buatista <rorens05@gmail.com>
2019-09-23 00:43:50 +0800

    update dev env

--- config/config.js
+++ config/config.js
@@ -2,10 +2,10 @@ require('dotenv').config()
 
 module.exports = {
   "development": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME,
-    "host": process.env.DB_HOST,
+    "username": process.env.DEV_DB_USERNAME,
+    "password": process.env.DEV_DB_PASSWORD,
+    "database": process.env.DEV_DB_NAME,
+    "host": process.env.DEV_DB_HOST,
     "dialect": "mysql"
   },
   "test": {
